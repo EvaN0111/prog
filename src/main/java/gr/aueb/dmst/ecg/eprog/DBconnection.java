bafe55f4cd1fcd72ac8d76891047483f68705560
EvaNtziou <148814179+EvaN0111@users.noreply.github.com>
2024-01-15 20:11:06 +0200

    src

---
+++ src/main/java/gr/aueb/dmst/ecg/eprog/DBconnection.java
@@ -0,0 +1,19 @@
+package gr.aueb.dmst.ecg.eprog;
+
+import java.sql.Connection;
+
+public class DBconnection {
+
+    public void dbcon() {
+        // ensure the connection with the database
+        final Connection con;
+        try {
+            con = DB.openConnection();
+            DB.createTables(con);
+            DB.close();
+
+        } catch (Exception e) {
+            System.err.println("There has been a problem establishing a connection with the Database.");
+        }
+    }
+}
