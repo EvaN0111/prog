6a42e1ecbec319b7c73721d4e57eb737c1abacde
aggelikidep <147502641+aggelikidep@users.noreply.github.com>
2024-01-17 19:02:48 +0200

    Add files via upload

--- src/test/java/gr/aueb/dmst/ecg/eprog/AIModelTest2.java
+++ src/test/java/gr/aueb/dmst/ecg/eprog/AIModelTest2.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
+
+import gr.aueb.dmst.ecg.eprog.AIModel.AIException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 public class AIModelTest2 {
 
@@ -12,8 +15,16 @@ public class AIModelTest2 {
     public void testGenAnswer() {
         AIModel aiModel = new AIModel();
         String apikey = System.getenv("API_KEY");
-        String result = aiModel.genAnswer(null, null, null);
+        String result = null;
+        assertThat(aiModel).isNotNull();
+        try {
+            // Testing the answer generation with a valid API key
+            result = aiModel.genAnswer(null, null, null);
+        } catch (AIException a) {
+            
+        }
+        
         assertThat(result).isNotNull();
         System.out.println(result);
     }
-    }
+}
